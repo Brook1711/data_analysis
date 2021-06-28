4d03e57aa1e7ee1241dc9177da23f2d38cdb7fea
Brook1711 <33785776+Brook1711@users.noreply.github.com>
2021-06-28 18:12:26 +0800

    save

--- main.py
+++ main.py
@@ -13,10 +13,11 @@ class data_analysis:
         self.problem_num = 23
         self.df = df
         self.row_num = len(df)
-        self.ndf = 0
         self.df.insert(len(self.df.columns), 'ans', self.remove_str())
         self.df.insert(len(self.df.columns), 'interval', self.get_interval())
-        
+        self.ndf = pd.DataFrame(self.create_new_df())
+        self.ndf_list = self.divide_ndf()
+        self.group_list = self.group_by()
         print('init complete')
 
     def remove_str_per_row(self, data_per_row):
@@ -70,13 +71,46 @@ class data_analysis:
             twoD_list.append(ans_dic_list)
         return twoD_list
     
+    def divide_ndf(self):
+        ndf_list = []
+        for i in range(len(self.ndf.columns)):
+            ndf_list.append(pd.DataFrame(self.ndf.loc[:,i]))
+        return ndf_list
+    
+    def group_by_per_problem(self, index):
+        df_temp = self.ndf_list[index]
+        df_str_list = []
+        for j in range(len(df_temp)):
+            df_str_list.append(str(df_temp.iloc[j, 0]))
+        df_temp.insert(1, 'ans_str', df_str_list)
+        df_per_problom = df_temp.groupby('ans_str')
+        return df_per_problom
+
+    def group_by(self):
+        group_list = []
+        for i in range(self.problem_num):
+            df_temp = self.group_by_per_problem(i)
+            group_list.append(df_temp)
+        return group_list
+
     def plot(self):
-        s1 = np.random.RandomState(1)
-        x = s1.randn(1000)
-        data = [go.Histogram(x=x)] 
+        data = [go.Histogram(x=list(self.df.loc[:,'interval']))] 
         layout={"title": "学生用时分布", 
                                        "xaxis_title": "学生用时，单位秒",
-                                       "yaxis_title": "学生个数占比",
+                                       "yaxis_title": "学生个数",
+                                       # x轴坐标倾斜60度
+                                       "xaxis": {"tickangle": 60}
+                                      }
+        fig = go.Figure(data=data,layout=layout)
+        plot(fig,filename="./plot/vector.html",auto_open=False,image='png',image_height=800,image_width=1500)
+        offline.iplot(fig) 
+        return 0
+
+    def plot_problem(self):
+        data = [go.Bar(x = range(self.problem_num), y = [len(list(group)) for group in self.group_list])] 
+        layout={"title": "不同题目的编码数量", 
+                                       "xaxis_title": "题目编号",
+                                       "yaxis_title": "编码个数",
                                        # x轴坐标倾斜60度
                                        "xaxis": {"tickangle": 60}
                                       }
@@ -88,7 +122,5 @@ class data_analysis:
 if __name__ == '__main__':
     df = pd.read_excel('./data/data.xlsx')  
     data_entity = data_analysis(df)
-    data_entity.plot()
-    print(data_entity.df)
-    print(data_entity.remove_str())
-    print(len(data_entity.remove_str()))
+    a = data_entity.group_by()
+    print(np.shape(a))
